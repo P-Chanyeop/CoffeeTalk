c18cf2f0cca354508c74afe200074af296e3727a
Chanyeop <qkrcksduq1gh@naver.com>
2024-12-02 22:26:45 +0900

    프로젝트 업로드

---
+++ src/main/java/com/example/coffeetalk/CoffeeTalkApplication.java
@@ -0,0 +1,13 @@
+package com.example.coffeetalk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoffeeTalkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoffeeTalkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/coffeetalk/CoffeeTalkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.coffeetalk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoffeeTalkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
